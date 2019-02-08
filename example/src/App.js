74851e1660dfc00ff297e1081a150e7a4caa4f7f
sharafian <ben@sharafian.me>
2019-02-08 14:39:51 -0800

    feat: make the hooks work better

--- example/src/App.js
+++ example/src/App.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useWebMonetizationCounter } from 'react-web-monetization';
+// import { useWebMonetizationCounter } from 'react-web-monetization';
+import { useWebMonetizationCounter } from './counter';
+import { useWebMonetizationState } from './state';
+
+const ObjectTable = ({ obj }) => <table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    {Object.keys(obj).map(key => <tr>
+      <td>{key}</td>
+      <td>{String(obj[key])}</td>
+    </tr>)}
+  </tbody>
+</table>
 
 const App = () => {
   const monetizationDetails = useWebMonetizationCounter()
+  const monetizationState = useWebMonetizationState()
 
-  return <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        {JSON.stringify(monetizationDetails)}
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  return <div>
+    <h1>React Web Monetization</h1>
+
+    <h2>Web Monetization State</h2>
+    <ObjectTable obj={monetizationState} />
+
+    <h2>Web Monetization Counter</h2>
+    <ObjectTable obj={monetizationDetails} />
   </div>
 }
 
