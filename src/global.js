560989b377259e8c5788e51b3350d55a225da868
sharafian <ben@sharafian.me>
2019-02-25 16:56:21 -0800

    feat: init global wm state function

--- src/global.js
+++ src/global.js
@@ -69,3 +69,7 @@ export function getGlobalWebMonetizationState () {
   }
   return globalWebMonetizationState
 }
+
+export function initGlobalWebMonetizationState () {
+  getGlobalWebMonetizationState().init()
+}
