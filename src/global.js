40cdf774546a90e7cc8e222ea0eda6e9214a7685
sharafian <ben@sharafian.me>
2019-02-25 16:53:12 -0800

    feat: keep wm state in singleton

---
+++ src/global.js
@@ -0,0 +1,71 @@
+import { EventEmitter } from 'events'
+
+// TODO: is there a more elegant pattern for this?
+export class GlobalWebMonetizationState extends EventEmitter {
+  constructor () {
+    super()
+
+    this.state = document.monetization && document.monetization.state
+    this.paymentPointer = null
+    this.requestId = null
+    this.assetCode = null
+    this.assetScale = null
+    this.totalAmount = 0
+    this.initialized = false
+
+    this.boundMonetizationStart = this.onMonetizationStart.bind(this)
+    this.boundMonetizationProgress = this.onMonetizationProgress.bind(this)
+  }
+
+  getState () {
+    return {
+      state: this.state,
+      paymentPointer: this.paymentPointer,
+      requestId: this.requestId,
+      assetCode: this.assetCode,
+      assetScale: this.assetScale,
+      totalAmount: this.totalAmount
+    }
+  }
+
+  init () {
+    if (!this.initialized && document.monetization) {
+      this.initialized = true
+      document.monetization.addEventListener('monetizationstart', this.boundMonetizationStart)
+      document.monetization.addEventListener('monetizationprogress', this.boundMonetizationProgress)
+    }
+  }
+
+  terminate () {
+    if (this.initialized && document.monetization) {
+      this.initialized = false
+      document.monetization.removeEventListener('monetizationstart', this.boundMonetizationStart)
+      document.monetization.removeEventListener('monetizationprogress', this.boundMonetizationProgress)
+    }
+  }
+
+  onMonetizationStart (ev) {
+    const { paymentPointer, requestId } = ev.detail
+
+    this.paymentPointer = paymentPointer
+    this.requestId = requestId
+    this.emit('monetizationstart')
+  }
+
+  onMonetizationProgress (ev) {
+    const { amount, assetCode, assetScale } = ev.detail
+
+    this.totalAmount = this.totalAmount + Number(amount)
+    this.assetCode = assetCode
+    this.assetScale = assetScale
+    this.emit('monetizationprogress')
+  }
+}
+
+let globalWebMonetizationState
+export function getGlobalWebMonetizationState () {
+  if (!globalWebMonetizationState) {
+    globalWebMonetizationState = new GlobalWebMonetizationState()
+  }
+  return globalWebMonetizationState
+}
