e10eeeebe06e205c2ffc276aeac55355238dd83e
Nicholas Dudfield <ndudfield@gmail.com>
2019-10-29 14:02:47 +0700

    feat: add stopped state and pending/stop events

--- src/conditional.js
+++ src/conditional.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+
 import { useMonetizationState } from './state'
 
 export function IfWebMonetized({ children, showOnPending }) {
--- src/state.js
+++ src/state.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
+
 import { getGlobalWebMonetizationState } from './global'
 
 export function useMonetizationState() {
   // get the singleton WM state
   const webMonetizationState = getGlobalWebMonetizationState()
+
   webMonetizationState.init()
 
   const { state, requestId, paymentPointer } = webMonetizationState.getState()
@@ -16,19 +18,24 @@ export function useMonetizationState() {
   useEffect(() => {
     if (!document.monetization) return
 
-    const onStart = () => {
+    const stateChange = () => {
       const {
         state,
         requestId,
         paymentPointer
       } = webMonetizationState.getState()
+
       setMonetizationState({ state, requestId, paymentPointer })
     }
 
-    webMonetizationState.on('monetizationstart', onStart)
+    webMonetizationState.on('monetizationpending', stateChange)
+    webMonetizationState.on('monetizationstart', stateChange)
+    webMonetizationState.on('monetizationstop', stateChange)
 
     return () => {
-      webMonetizationState.removeListener('monetizationstart', onStart)
+      webMonetizationState.removeListener('monetizationstart', stateChange)
+      webMonetizationState.removeListener('monetizationpending', stateChange)
+      webMonetizationState.removeListener('monetizationstop', stateChange)
     }
   })
 
